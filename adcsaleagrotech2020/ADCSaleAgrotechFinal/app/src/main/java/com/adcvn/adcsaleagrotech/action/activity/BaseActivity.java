a5e3f7016c56345740cefb190c1ef587aa798fc3
ThachThanhTuan <thachthanhtuanit@gmail.com>
2020-08-20 21:44:52 +0700

    - hong lanh cap nhat code moi

--- adcsaleagrotech2020/ADCSaleAgrotechFinal/app/src/main/java/com/adcvn/adcsaleagrotech/action/activity/BaseActivity.java
+++ adcsaleagrotech2020/ADCSaleAgrotechFinal/app/src/main/java/com/adcvn/adcsaleagrotech/action/activity/BaseActivity.java
@@ -6,9 +6,7 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
-//hong lanh thay doi code lan 2
-//hong lanh thay doi code lan 1
-//hong lanh thay doi code
+//hong lanh thay doi code moi
 public class BaseActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
