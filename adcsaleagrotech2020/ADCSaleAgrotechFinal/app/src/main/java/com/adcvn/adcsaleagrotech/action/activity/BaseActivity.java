f4e4ebae7e20b4c5f584c2c80a69a42bce418c9c
Thach Thanh Tuan <thachanhtuan@adcvn.com>
2020-08-20 15:08:27 +0700

    - Tuấn thay đổi tí

--- adcsaleagrotech2020/ADCSaleAgrotechFinal/app/src/main/java/com/adcvn/adcsaleagrotech/action/activity/BaseActivity.java
+++ adcsaleagrotech2020/ADCSaleAgrotechFinal/app/src/main/java/com/adcvn/adcsaleagrotech/action/activity/BaseActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
+// THAY ĐỔI CHƠI
 // BASE NÀO ĐÓ
 public class BaseActivity extends AppCompatActivity {
     @Override
