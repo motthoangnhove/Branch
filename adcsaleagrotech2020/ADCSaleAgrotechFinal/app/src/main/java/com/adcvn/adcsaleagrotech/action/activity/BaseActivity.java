d119ff849cdbd479629e7f2e7b8f55874e663df6
Thach Thanh Tuan <thachanhtuan@adcvn.com>
2020-08-20 21:26:21 +0700

    - thanh tuan cap nhat code lan 2

--- adcsaleagrotech2020/ADCSaleAgrotechFinal/app/src/main/java/com/adcvn/adcsaleagrotech/action/activity/BaseActivity.java
+++ adcsaleagrotech2020/ADCSaleAgrotechFinal/app/src/main/java/com/adcvn/adcsaleagrotech/action/activity/BaseActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
+// thanh tuan thay doi code lan 2
 // thanh tuan cap nhat code lan 1
 // thanh tuan thay doi code moi
 public class BaseActivity extends AppCompatActivity {
