0a4ac897d98e82be8fa2c58a0833c110fda86d4b
ThachThanhTuan <thachthanhtuanit@gmail.com>
2020-08-20 15:54:57 +0700

    - thay doi  tong hop

--- adcsaleagrotech2020/ADCSaleAgrotechFinal/app/src/main/java/com/adcvn/adcsaleagrotech/action/activity/BaseActivity.java
+++ adcsaleagrotech2020/ADCSaleAgrotechFinal/app/src/main/java/com/adcvn/adcsaleagrotech/action/activity/BaseActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
-//lanh thay doi ne
 public class BaseActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
